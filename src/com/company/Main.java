5d4b016138fd820be03819158f3348c6a680a6f8
Fornitiedde <tim.suter@hotmail.com>
2018-05-06 11:09:24 +0200

    hblbl

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,12 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 	// write your code here
-        System.out.println("Hblbl");
         Page test;
         test = new Page();
-        test.setLink("Hblbl");
-        test.printLink();
-        test.add("test1");
-        test.add("test2");
+        test.setLink("http://www.brainjar.com/java/host/test.html");
+        test.read();
         test.printList();
     }
 }
--- src/com/company/Page.java
+++ src/com/company/Page.java
@@ -1,6 +1,9 @@
 package com.company;
 
+import java.util.Scanner;
 import java.util.ArrayList;
+import java.net.*;
+
 
 public class Page {
     private String Link;
@@ -35,9 +38,21 @@ public class Page {
 
     //Print List
     public void printList(){
+        System.out.println("StringList: ");
         for(int i=0; i<this.StringList.size(); i++){
             System.out.println(this.StringList.get(i));
         }
     }
 
+    //
+    public void read() throws Exception{
+        URL u;
+        Scanner s;
+            u = new URL(this.Link);
+            s = new Scanner(u.openStream());
+
+            while(s.hasNext()){
+                this.StringList.add(s.nextLine());
+            }
+    }
 }
